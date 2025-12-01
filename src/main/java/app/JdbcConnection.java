b04a1010ba70b4dde13ab109d3f8ed94e2806971
Mitia Jessica <mitiah.jessica@gmail.com>
2025-12-01 14:34:42 +0300

    feat: add the Main class

--- src/main/java/app/JdbcConnection.java
+++ src/main/java/app/JdbcConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class JdbcConnection {
-    private  static final String URL = "jdbc:postgresql://localhost:5432/";
+    private  static final String URL = "jdbc:postgresql://localhost:5432/student";
     private  static final String USER = "postgres";
     private  static final String PASSWORD = "jessica123";
 
